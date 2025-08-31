c5fb46177dea7b8db8cf3c259905fe9ee6f47348
moha528 <mtseckdev@gmail.com>
2025-08-31 00:34:28 +0000

    security config

--- src/main/java/sn/zeitune/oliveinsuranceinsured/config/BeansConfig.java
+++ src/main/java/sn/zeitune/oliveinsuranceinsured/config/BeansConfig.java
@@ -18,19 +18,6 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class BeansConfig {
 
-
-    @Bean
-    public CorsFilter corsFilter() {
-        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        final CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true);
-        config.setAllowedOriginPatterns(Collections.singletonList("*"));
-        config.setAllowedHeaders(Collections.singletonList("*"));
-        config.setAllowedMethods(Arrays.stream(HttpMethod.values()).map(HttpMethod::name).collect(Collectors.toList()));
-        source.registerCorsConfiguration("/**", config);
-        return new CorsFilter(source);
-    }
-
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
